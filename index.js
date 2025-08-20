9f66052bb6b74c350b363b9478da6cb284977eba
Md. Shoyaif Rahman <sohan0020p@gmail.com>
2025-08-21 01:29:41 +0600

    added delete feature

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 const like = document.getElementById('like');
 const comment = document.getElementById('comment');
 const share = document.getElementById('share');
@@ -7,38 +5,48 @@ const likeCount = document.getElementById('likeCount');
 const commentCount = document.getElementById('commentCount');
 const shareCount = document.getElementById('shareCount');
 
-let cntComment = 0;
-
-like.addEventListener('click',function(){
-
-//   console.log(++cnt);
 let cntLike = 0;
-    likeCount.innerText=++cntLike;
-  
-})
-
-comment.addEventListener('click',function(){
-const input = document.getElementById('input');
-const span = document.createElement('span');
-span.classList.add('text-lg','px-2', 'bg-gray-600','text-white','mb-2', 'rounded-md')
-
-    const getInput = input.value;
-    console.log(getInput);
-    commentCount.innerText=++cntComment;
-  const newDiv = document.getElementById('newDiv');
-  span.innerText = getInput;
-  newDiv.appendChild(span);
-  newDiv.appendChild(document.createElement('br'));
-
-input.value = "";
-})
-
-
-
-share.addEventListener('click',function(){
-
-//   console.log(++cnt);
+let cntComment = 0;
 let cntShare = 0;
-    shareCount.innerText=++cntShare;
-  
-})
+
+like.addEventListener('click', function () {
+    likeCount.innerText = ++cntLike;
+});
+
+comment.addEventListener('click', function () {
+    const input = document.getElementById('input');
+    const getInput = input.value.trim();
+
+    if (getInput) {
+        const span = document.createElement('span');
+        span.classList.add(
+            'text-lg',
+            'px-2',
+            'bg-gray-600',
+            'text-white',
+            'mb-2',
+            'rounded-md'
+        );
+
+        span.innerText = getInput;
+
+        const newDiv = document.getElementById('newDiv');
+        newDiv.appendChild(span);
+        newDiv.appendChild(document.createElement('br'));
+
+        commentCount.innerText = ++cntComment;
+
+        input.value = "";
+    }
+});
+
+share.addEventListener('click', function () {
+    shareCount.innerText = ++cntShare;
+});
+
+
+document.getElementById("newDiv").addEventListener("click", function (event) {
+    if (event.target.tagName === "SPAN") {
+        event.target.remove();
+    }
+});
